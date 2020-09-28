164ce4030acd23931ecdea3d4afac2235a31ce77
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-28 15:39:39 -0500

    added my favorite number variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static  void main(String[] args){
-        System.out.print("Hello, ");
-        System.out.print("World");
+//        System.out.print("Hello, ");
+//        System.out.print("World");
+        int myFavoriteNumber = 27;
+        System.out.println(myFavoriteNumber);
     }
 }
