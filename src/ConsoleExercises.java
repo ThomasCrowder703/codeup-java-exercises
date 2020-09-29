c86c7b592a0a7580974cf12d49dcf59ee0399912
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-29 13:59:13 -0500

    user adds three words

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args){
-        double pi = 3.14159;
-
-        System.out.printf("The value of pi is approximately %.2f\n", pi);
-
+//        double pi = 3.14159;
+//
+//        System.out.printf("The value of pi is approximately %.2f\n", pi);
+//
         Scanner sc = new Scanner(System.in);
-        System.out.print("Please enter an Integer.");
+//        System.out.print("Please enter an Integer.");
+//
+//        int userInt = sc.nextInt();
+//        System.out.printf("Your number is %d", userInt);
 
-        int userInt = sc.nextInt();
-        System.out.printf("Your number is %d", userInt);
+        System.out.println("Please enter three words");
+        String firstWord = sc.next();
+        String secondWord = sc.next();
+        String thirdWord = sc.next();
+        System.out.printf("Your words are: \n %s \n %s \n %s", firstWord, secondWord, thirdWord);
     }
 }
