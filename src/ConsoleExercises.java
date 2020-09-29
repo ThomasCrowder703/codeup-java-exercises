52c7ec97213ed1f843ddd88f5f6b005bba5c3043
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-29 15:53:49 -0500

    walktrhough version of last question

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,29 +6,35 @@ public class ConsoleExercises {
 //
 //        System.out.printf("The value of pi is approximately %.2f\n", pi);
 //
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
+//
 //        System.out.print("Please enter an Integer.");
 //
 //        int userInt = sc.nextInt();
-//        System.out.printf("Your number is %d", userInt);
-
+//        System.out.printf("Your number is %d\n", userInt);
+//
 //        System.out.println("Please enter three words");
 //        String firstWord = sc.next();
 //        String secondWord = sc.next();
 //        String thirdWord = sc.next();
-//        System.out.printf("Your words are: \n %s \n %s \n %s", firstWord, secondWord, thirdWord);
-
+//        System.out.printf("Your words are: \n %s \n %s \n %s\n", firstWord, secondWord, thirdWord);
+//
+//        Scanner sentence = new Scanner(System.in);
 //        System.out.println("Please enter a sentence");
-//        String userSentence =  sc.nextLine();
+//        String userSentence =  sentence.nextLine();
 //        System.out.printf("%s", userSentence);
 
+        Scanner calculatePerimeter = new Scanner(System.in);
+        System.out.println("Please enter the length of the codeup classroom");
+        int length = Integer.parseInt(calculatePerimeter.nextLine());
 
-        System.out.println("Please enter the length and width of the codeup classroom");
-        String length = sc.next();
-        String width = sc.next();
-        int perimeter = (Integer.parseInt(length)*2) + (Integer.parseInt(width)*2);
-        System.out.printf("The perimeter of the classroom is %d", perimeter);
+        System.out.println("Please enter the width of the classroom");
+         int width = Integer.parseInt(calculatePerimeter.nextLine());
 
+         int area = length * width;
+         int perimeter = (length * 2) + (width * 2);
+
+        System.out.printf("The area is %d and the perimeter is %d", area, perimeter);
     }
 
 }
