f92eb2e9a65cfedcaf37c2e57d2734c65dfae4a5
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-29 13:54:24 -0500

    added user input functionality

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args){
         double pi = 3.14159;
 
-        System.out.printf("The value of pi is approximately %.2f", pi);
+        System.out.printf("The value of pi is approximately %.2f\n", pi);
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter an Integer.");
+
+        int userInt = sc.nextInt();
+        System.out.printf("Your number is %d", userInt);
     }
 }
