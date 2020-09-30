2fab357dab00661a7f4129b27c7251e65e5239c3
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-30 09:19:00 -0500

    created a do while in java

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,12 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println(i++);
+//        }
+        int i = 0;
+        do {
+            System.out.println(i+=2);
+        }while (i<100);
+    }
+}
