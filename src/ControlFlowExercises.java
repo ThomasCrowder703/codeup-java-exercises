9c8eb4fd6aa39d487b9f246f02c3b46143d35bb3
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-30 09:51:22 -0500

    refactored two previous exercises into for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -9,10 +9,18 @@ public class ControlFlowExercises {
 //        do {
 //            System.out.println(i -= 5);
 //        }while (i > -10);
-        int i = 2;
-        do{
+//        int i = 2;
+//        do{
+//            System.out.println(i);
+//            i = (int) Math.pow(i,2);
+//        }while (i < 1000000);
+
+//        for (int i = 5; i<=15;i++){
+//            System.out.println(i);
+//        }
+
+        for(int i=2; i < 1000000; i = (int) Math.pow(i,2)){
             System.out.println(i);
-            i = (int) Math.pow(i,2);
-        }while (i < 1000000);
+        }
     }
 }
