c85ef99f3b75516febf36ddd6f167eb5b7f95fa2
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-30 09:47:45 -0500

    added the ability to square on a do while loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,9 +4,15 @@ public class ControlFlowExercises {
 //        while (i <= 15){
 //            System.out.println(i++);
 //        }
-        int i = 0;
-        do {
-            System.out.println(i+=2);
-        }while (i<100);
+//        int i = 100;
+//        System.out.print(i +"\n");
+//        do {
+//            System.out.println(i -= 5);
+//        }while (i > -10);
+        int i = 2;
+        do{
+            System.out.println(i);
+            i = (int) Math.pow(i,2);
+        }while (i < 1000000);
     }
 }
