936d0852409c2ad2c35f5740f320deac88bed7e7
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-01 14:02:00 -0500

    added table

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Formatter;
+
 public class ControlFlowExercises {
     public static void main(String[] args){
 //        int i = 5;
@@ -23,19 +26,42 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for(int i=1; i<=100; i++){
-            if(i % 3 == 0){
-                System.out.println("Fizz");
+//        for(int i=1; i<=100; i++){
+//            if(i % 3 == 0){
+//                System.out.println("Fizz");
+//            }
+//            if(i % 5 == 0){
+//                System.out.println("Buzz");
+//            }
+//            if(i % 5 == 0 && i % 3 == 0){
+//                System.out.println("FIzzBuzz");
+//            }
+//            if (i % 3 != 0 && i % 5 != 0){
+//                System.out.println(i);
+//            }
+//        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter an integer.");
+        int userInteger = sc.nextInt();
+
+
+        System.out.println("Here is your table: ");
+        System.out.println(" ");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+        for(int i = 1; i <= userInteger; i++){
+            int squared = (int) Math.pow(i,2);
+            int cubed = (int) Math.pow(i,3);
+            if(i < 4){
+                System.out.println(i + "      |" + squared + "        |" + cubed);
+            } else {
+                System.out.println(i + "      |" + squared + "       |" + cubed);
             }
-            if(i % 5 == 0){
-                System.out.println("Buzz");
-            }
-            if(i % 5 == 0 && i % 3 == 0){
-                System.out.println("FIzzBuzz");
-            }
-            if (i % 3 != 0 && i % 5 != 0){
-                System.out.println(i);
-            }
+
         }
+
+
+
     }
 }
