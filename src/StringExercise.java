0c20c829e59d91567babcc7b4ef369257c970053
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-02 10:29:15 -0500

    basic string exercises

---
+++ src/StringExercise.java
@@ -0,0 +1,15 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String pinkFloyd = "We don't need no education.\n" + "We don't need no thought control";
+        System.out.println(pinkFloyd);
+
+        String punctuation = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(punctuation);
+
+        String backSlash = "In windows, the main drive is usually C :\\";
+        System.out.println(backSlash);
+
+        String manyBackslash = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\!";
+        System.out.println(manyBackslash);
+    }
+}
