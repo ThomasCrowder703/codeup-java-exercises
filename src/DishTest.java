d52dcea262d40eb1b03a9ab8a825e09791513e9e
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-05 14:16:25 -0500

    added class DishTest

---
+++ src/DishTest.java
@@ -0,0 +1,9 @@
+public class DishTest {
+    public static void main(String[] args) {
+        Dish dish1 = new Dish();
+        dish1.costInCents = 30;
+        dish1.nameOfDish =  "Mushroom Soup";
+        dish1.wouldRecommend = false;
+        dish1.printSummary();
+    }
+}
