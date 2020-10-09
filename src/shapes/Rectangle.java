630ac924e9724d622537b0f09e78ad572b127805
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-09 12:19:52 -0500

    created measurable interface/ abstract shapes class/ abstract quad class

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,20 +1,5 @@
 package shapes;
 
 public class Rectangle {
-    protected int length;
-    protected int width;
 
-    public Rectangle(int lengthInput, int widthInput){
-        this.length = lengthInput;
-        this.width = widthInput;
-    }
-
-    public int getArea(){
-        return this.length * this.width;
-    }
-
-
-    public int getPerimeter(){
-        return (this.length*2) + (this.width * 2);
-    }
 }
