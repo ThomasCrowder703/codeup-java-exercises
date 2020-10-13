fbf9b395efce7ddd83c49be9264124cb42dc9113
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-13 11:45:34 -0500

    experimented with indexOf

--- src/collections/CollectionsLec.java
+++ src/collections/CollectionsLec.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class CollectionsLec {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
+
         System.out.println(names.size()); //returns 0 value
         names.add("Fernando");
         System.out.println(names.size()); //returns 1 value
