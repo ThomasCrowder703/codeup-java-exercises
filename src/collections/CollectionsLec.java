302320a337fc12bd4c002eadd10d8ed3c1de88ad
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-10-13 09:37:26 -0500

    used get/indexOf in collections

--- src/collections/CollectionsLec.java
+++ src/collections/CollectionsLec.java
@@ -7,8 +7,21 @@ public class CollectionsLec {
         System.out.println(names.size()); //returns 0 value
         names.add("Fernando");
         System.out.println(names.size()); //returns 1 value
+        names.add("Daniel");
+        names.add("Kenneth");
 
         System.out.println(names.get(0));
-        System.out.println(names.get(1));// does not exist
+
+        System.out.println(names.indexOf("Kenneth")); // returns 2
+
+        System.out.println(names.get(2).toUpperCase()); // returns "KENNETH"
+        System.out.println(names.get(names.indexOf("Kenneth")).toUpperCase());// returns "KENNETH"
+
+        if(names.contains("Kenneths")) {
+            System.out.println(names.get(names.indexOf("Kenneth")).toUpperCase());
+        } //will not print if false instead of breaking the app
+
+
+
     }
 }
